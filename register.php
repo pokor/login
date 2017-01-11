2742b5547231cc1a6d35193ab99e4ca111a47837
pokor <pokorzhang@gmail.com>
2017-01-12 00:09:53 +0800

    新更改的注册页面

--- register.php
+++ register.php
@@ -45,7 +45,7 @@ if($username == "" || $password == "" || $pwd_again == "")
         }
     }
 }*/
-session_start();
+/*session_start();
 include_once "vendor/autoload.php";
 $config = new \Doctrine\DBAL\Configuration();
 if(!$_POST){
@@ -71,7 +71,7 @@ if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['pass
         if(strlen($pass)>= 6 && strlen($pass)<18){
             if($pass != $pawd){
                 echo"两次输入的密码不一致,请重新输入！"."<br/><br/>";
-                echo"<a href='login.html'>重新输入</a>";
+                echo"<a href='login.PHP'>重新输入</a>";
                 die();
             }else{
                 $data = [];
@@ -82,25 +82,83 @@ if(isset($_POST['username']) && !empty($_POST['username']) && isset($_POST['pass
                     echo"注册成功!"."<br/><br/>";
                     $_SESSION['username'] = $name;
                     $_SESSION['password'] = $pass;
-                    echo"<a href='login.html'>立刻登录</a>";
+                    echo"<a href='login.php'>立刻登录</a>";
                     die();
                 }else{
                     echo"注册不成功！"."<br/><br/>";
-                    echo "<a href='views/login-in.html'>返回</a>";
+                    echo "<a href='views/register.php'>返回</a>";
                     die();
                 }
             }
         }else{
             echo "<script>alert('密码不能小于6位或小于18位')</script>";
-            echo "<a href='views/login-in.html'>返回</a>";
+            echo "<a href='views/register.php'>返回</a>";
         }
     }else{
         echo "<script>alert('用户名不能小于6位数或者大于18位')</script>";
-        echo "<a href='views/login-in.html'>返回</a>";
+        echo "<a href='views/register.php'>返回</a>";
     }
 
 }else{
     echo "<script>alert(\"请确认信息完整性\")</script>";
-    echo "<a href='views/login-in.html'>返回</a>";
+    echo "<a href='views/register.php'>返回</a>";
 
+}*/
+$method = $_SERVER['REQUEST_METHOD'];
+if ($method == 'GET'){
+    require_once 'views/login.php';
+
+}elseif ($method == 'POST'){
+    $verify = false;
+    $username = null;
+    $password = null;
+    $pasd = null;
+    if (isset($_POST['username'])){
+        $username = $_POST['username'];
+    }
+    if (isset($_POST['password'])){
+        $password = $_POST['password'];
+    }
+    if (isset($_POST['pwd-again'])){
+        $pasd = $_POST['pwd-again'];
+    }
+    if ($password == $pasd){
+        if(strlen($username)>6 && strlen($username)<18){
+            if(strlen($password)>= 6 && strlen($password)<18){
+                if($password != $pasd){
+                    echo"两次输入的密码不一致,请重新输入！"."<br/><br/>";
+                    echo"<a href='login.PHP'>重新输入</a>";
+                    die();
+                }else{
+                    $data = [];
+                    $data['username'] = trim($username);
+                    $data['password'] = md5(trim($password));
+                    $par = [
+                        'dbname' => 'hello',
+                        'user' => 'root',
+                        'password' => 'root',
+                        'host' => 'localhost',
+                        'driver' => 'mysqli',
+                    ];
+                    $conn = \Doctrine\DBAL\DriverManager::getConnection($par,$config);
+                    $res = $conn->insert('user',$conn);
+                    if($res>0){
+                        echo"注册成功!"."<br/><br/>";
+                        echo"<a href='views/login.php'>立刻登录</a>";
+                        die();
+                    }else{
+                        echo"注册不成功！"."<br/><br/>";
+                        echo "<a href='views/register.php'>返回</a>";
+                        die();
+                    }
+                }
+            }else{
+                echo "<script>alert('密码不能小于6位或小于18位')</script>";
+                echo "<a href='views/register.php'>返回</a>";
+            }
+        }else{
+            echo "<script>alert('用户名不能小于6位数或者大于18位')</script>";
+            echo "<a href='views/register.php'>返回</a>";
+        }
+    }
 }
